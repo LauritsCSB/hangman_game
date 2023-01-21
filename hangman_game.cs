e27a699c5615dd129d96d46289da5044a859571e
Laurits Bentsen <laurits2@hotmail.com>
2023-01-21 13:51:40 +0100

    do while loop wrapped around main variables and loop. Continues do run central code list as long as user want to. Removed method call for restarting app.

--- hangman_game.cs
+++ hangman_game.cs
@@ -9,95 +9,85 @@ namespace hangmanGame
 
         static void Main(string[] args)
         {
-            /*
-            The program runs by generating a random number and using that to pick a word from the array at a random index.
-            As long as the "empty" guessing string isn't equal to the word pick from the array, the while loop runs.
-            If the user runs out of lives or if the word is guessed correctly, the while loop breaks.
-            A feature was added to check for whitespace and multiple characters in the same input, this wont be taken as a
-            wrong answer and the user gets a second chance.
-            */
-            Start();
-        }
-
-        public static void Start()
-        {
             string[] wordsArray = { "property", "departure", "disk", "possession", "assignment", "goal", "year", "impression", "university", "information",
                 "operation", "piano", "county", "woman", "negotiation", "son", "thing", "girl", "sympathy", "volume", "mood", "funeral", "administration",
                 "economics", "role" };
 
-            int userLives = USER_LIVES;
+
             Random randomNumber = new Random();
-            int wordIndex = randomNumber.Next(wordsArray.Length);
-            string secretWord = wordsArray[wordIndex];
+            string playOrNo = "x";
             string userGuess;
-            List<string> wrongGuesses = new List<string>();
 
-            StringBuilder guessingString = new StringBuilder(secretWord.Length);
-            guessingString.Append('_', secretWord.Length);
 
-            Console.WriteLine($"Hello user!\nThis is a hangman game. The program lets you guess the letters in a randomly chosen word.\n" +
+            do
+            {
+                Console.Clear();
+
+                int userLives = USER_LIVES;
+
+                int wordIndex = randomNumber.Next(wordsArray.Length);
+                string secretWord = wordsArray[wordIndex];
+
+                StringBuilder guessingString = new StringBuilder(secretWord.Length);
+                guessingString.Append('_', secretWord.Length);
+
+                List<string> wrongGuesses = new List<string>();
+
+                Console.WriteLine($"Hello user!\nThis is a hangman game. The program lets you guess the letters in a randomly chosen word.\n" +
                 $"You have {userLives} wrong guesses untill the noose tightens and you loose the game!\n" +
                 $"You can't use whitespace as input and you can only input one letter at a time. Have fun!");
 
-            while (!guessingString.Equals(secretWord) && userLives > 1)
-            {
-
-                Console.WriteLine("Please enter your guess and press enter: ");
-
-                userGuess = Console.ReadLine().ToLower();
-
-                if (wrongGuesses.Contains(userGuess))
+                while (!guessingString.Equals(secretWord) && userLives > 1)
                 {
-                    Console.WriteLine("You already tried that!");
-                    continue;
-                }
-                else if (secretWord.Contains(userGuess))
-                {
-                    for (int i = 0; i < secretWord.Length; i++)
+
+                    Console.WriteLine("Please enter your guess and press enter: ");
+
+                    userGuess = Console.ReadLine().ToLower();
+
+                    if (wrongGuesses.Contains(userGuess))
                     {
-                        if (userGuess.StartsWith(secretWord[i]))
+                        Console.WriteLine("You already tried that!");
+                        continue;
+                    }
+                    else if (secretWord.Contains(userGuess))
+                    {
+                        for (int i = 0; i < secretWord.Length; i++)
                         {
-                            guessingString.Replace("_", userGuess, i, 1);
+                            if (userGuess.StartsWith(secretWord[i]))
+                            {
+                                guessingString.Replace("_", userGuess, i, 1);
+                            }
                         }
                     }
+                    else if (String.IsNullOrWhiteSpace(userGuess) || userGuess.Length > 1)
+                    {
+                        Console.WriteLine("Wrong input, try again!");
+                        continue;
+                    }
+                    else
+                    {
+                        userLives--;
+                        wrongGuesses.Add(userGuess);
+                        Console.WriteLine($"Wrong answer! You have {userLives} lives left.");
+                    }
+
+                    Console.Write($"Your guess: {guessingString}\nWrong guesses: ");
+                    Console.WriteLine(string.Join(", ", wrongGuesses));
                 }
-                else if (String.IsNullOrWhiteSpace(userGuess) || userGuess.Length > 1)
+
+                if (guessingString.Equals(secretWord))
                 {
-                    Console.WriteLine("Wrong input, try again!");
-                    continue;
+                    Console.WriteLine($"Congratulations! you guessed it.");
                 }
                 else
                 {
-                    userLives--;
-                    wrongGuesses.Add(userGuess);
-                    Console.WriteLine($"Wrong answer! You have {userLives} lives left.");
+                    Console.WriteLine($"Sorry, you lost!\nThe right answer was {secretWord}.");
                 }
 
-                Console.Write($"Your guess: {guessingString}\nWrong guesses: ");
-                Console.WriteLine(string.Join(", ", wrongGuesses));
-            }
+                Console.WriteLine("Do you want to play again? Type 'y' for yes and anything else for no, then press enter.");
+                playOrNo = Console.ReadLine().ToLower();
 
-            if (guessingString.Equals(secretWord))
-            {
-                Console.WriteLine($"Congratulations! you guessed it.");
-            }
-            else
-            {
-                Console.WriteLine($"Sorry, you lost!\nThe right answer was {secretWord}.");
-            }
-
-            Console.WriteLine("Do you want to play again? Type 'y' for yes and anything else for no, then press enter.");
-            string playOrNot = Console.ReadLine().ToLower();
-
-            if (playOrNot.Contains("y"))
-            {
-                Console.Clear();
-                Start();
-            }
-            else
-            {
-                Console.WriteLine("Bye!");
-            }
+            } while (playOrNo.Contains("y"));
         }
     }
 }
